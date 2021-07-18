08be0130595f318423ba2145bdc5276cf32a47da
ztex010073218 <ztex010073218@gmail.com>
2021-07-18 14:21:00 +0800

    ROI註解

--- DefectInspector/ROI.cpp
+++ DefectInspector/ROI.cpp
@@ -4,11 +4,12 @@ using namespace std;
 using namespace cv;
 
 
-ROI::ROI()
+ROI::ROI()//ROI的建構子
 {
 	ROI::img = Mat(Size(1000, 1000), CV_8UC3,Scalar(0,255,0));
 }
 
+//更改篩選條件時，更改底色
 void ROI::change_background(const Data_type& type)
 {
 	switch (type)
@@ -22,6 +23,7 @@ void ROI::change_background(const Data_type& type)
 	}
 }
 
+//搜尋已儲存的圖片(未加入功能)
 bool ROI::seach_jpg(const string &target)
 {
 	if (haveImageReader(target))
@@ -33,11 +35,12 @@ bool ROI::seach_jpg(const string &target)
 		return false;
 }
 
+//繪製ROI
 Mat ROI::show(const vector<paint_unit> &data, const int &level)
 {
-	ROI::img = background;
+	ROI::img = background;//繪製底色
 	int width;
-	switch (level) {
+	switch (level) {//根據不同level時，設定繪製方寬框的邊長
 	case 0:
 		width = 1;
 		break;
@@ -47,7 +50,7 @@ Mat ROI::show(const vector<paint_unit> &data, const int &level)
 	case 2:
 		width = 100;
 	}
-	for(int i=0;i<data.size();i++)
+	for(int i=0;i<data.size();i++)//繪製Dies
 		rectangle(img, Rect(data[i].paintx * width, data[i].painty * width, width, width),data[i].color,-1);
 	return img;
 }
--- DefectInspector/ROI.h
+++ DefectInspector/ROI.h
@@ -1,8 +1,4 @@
 #pragma once
-//#include <opencv2/highgui.hpp>
-//#include <opencv2/opencv.hpp>
-//#include <string>
-//#include <vector>
 #include "DataController.h"
 
 using namespace std;
@@ -10,11 +6,11 @@ using namespace std;
 class ROI
 {
 private:
-	cv::Mat img;
-	cv::Scalar background = cv::Scalar(0, 255, 0);//defeat background color is green
+	cv::Mat img;//繪製用的陣列
+	cv::Scalar background = cv::Scalar(0, 255, 0);//設定背景顏色的變數，預設綠色
 public:
-	ROI();
-	void change_background(const Data_type&);
-	cv::Mat show(const vector<paint_unit>&,const int&);
-	bool seach_jpg(const string&);//searh stored img
+	ROI();//ROI的建構子
+	void change_background(const Data_type&);//更改篩選條件時，更改底色
+	cv::Mat show(const vector<paint_unit>&,const int&);//繪製ROI
+	bool seach_jpg(const string&);//搜尋已儲存的圖片(未加入功能)
 };
